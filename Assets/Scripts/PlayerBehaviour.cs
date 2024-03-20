eed26ee52141f246b6fd3f67a2d77b84544e9c84
Rubiio14 <147033219+Rubiio14@users.noreply.github.com>
2024-03-20 13:51:32 +0000

    1.0.7
    Cambios en el player behaviour , raycast aplicado para corregir antravesado de arboles

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -4,75 +4,90 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
-	
-	public SwipeController m_SwipeController;
+    public SwipeController m_SwipeController;
 
-	public float m_Offset = 100f;
+    public float m_Offset = 100f;
     public float m_Duration = 1f;
     public GameObject m_Player;
     public bool m_CanJump = true;
-	Vector3 previousPosition;
-    public void Awake()
-	{
-		m_Player = this.gameObject;
-	}
 
-	public void Start()
-	{
+    public void Awake()
+    {
+        m_Player = this.gameObject;
+    }
+
+    public void Start()
+    {
         m_SwipeController.OnMovement += MoveTarget;
     }
 
 
     public void OnDisable()
-	{
-        m_SwipeController.OnMovement -= MoveTarget;   
-    }
-
-	void MoveTarget(Vector3 m_Direction)
     {
-        if (m_CanJump == true)
-        {
-            if (m_Direction.x > 0)
-            {
-                transform.eulerAngles = new Vector3(0, -90f, 0);
-            }
-            else if (m_Direction.x < 0)
-            {
-                transform.eulerAngles = new Vector3(0, 90f, 0);
-            }
-            else if (m_Direction.z > 0)
-            {
-                transform.eulerAngles = new Vector3(0, 180f, 0);
-            }
-            else if (m_Direction.z < 0)
-            {
-                transform.eulerAngles = new Vector3(0, 0, 0);
-            }
-
-            LeanTween.move(m_Player, m_Player.transform.position + new Vector3(m_Direction.normalized.x / 2, 0, 0) + Vector3.up / 2, m_Duration / 2).setEase(LeanTweenType.easeOutQuad).setOnComplete(() =>
-            {
-                LeanTween.move(m_Player, m_Player.transform.position + new Vector3(m_Direction.normalized.x / 2, 0, 0) - Vector3.up / 2, m_Duration / 2).setEase(LeanTweenType.easeOutQuad);
-            });
-            m_CanJump = false;
-        }
-        
-
+        m_SwipeController.OnMovement -= MoveTarget;
     }
+
+    void Update()
+    {
+        // No necesitamos el Raycast en Update para este propósito
+    }
+
+    void MoveTarget(Vector3 m_Direction)
+    {
+        if (m_CanJump)
+        {
+            RaycastHit hitinfo;
+            Vector3 moveDirection = m_Direction.normalized;
+
+            // Check for obstacles in the movement direction
+            if (Physics.Raycast(transform.position + new Vector3(0, 1f, 0), m_MoveDirection, out m_Hitinfo, 1f))
+            {
+                Debug.Log("Hit Something, Restricting Movement");
+
+               
+                if (m_MoveDirection.x != 0)
+                {
+                    m_MoveDirection.x = 0;
+                }
+                
+            }
+
+            // Move according to the adjusted direction
+            if (m_MoveDirection != Vector3.zero)
+            {
+                if (m_MoveDirection.x > 0)
+                {
+                    transform.eulerAngles = new Vector3(0, -90f, 0);
+                }
+                else if (m_MoveDirection.x < 0)
+                {
+                    transform.eulerAngles = new Vector3(0, 90f, 0);
+                }
+                else if (m_MoveDirection.z > 0)
+                {
+                    transform.eulerAngles = new Vector3(0, 180f, 0);
+                }
+                else if (m_MoveDirection.z < 0)
+                {
+                    transform.eulerAngles = new Vector3(0, 0, 0);
+                }
+
+                LeanTween.move(m_Player, m_Player.transform.position + new Vector3(m_MoveDirection.x / 2, 0, 0) + Vector3.up / 2, m_Duration / 2).setEase(LeanTweenType.easeOutQuad).setOnComplete(() =>
+                {
+                    LeanTween.move(m_Player, m_Player.transform.position + new Vector3(m_MoveDirection.x / 2, 0, 0) - Vector3.up / 2, m_Duration / 2).setEase(LeanTweenType.easeOutQuad);
+                });
+
+                m_CanJump = false;
+            }
+        }
+    }
+
     public void OnCollisionEnter(Collision collision)
     {
-		if (collision.gameObject.tag == "Terrain" || collision.gameObject.tag == "ProceduralTerrain")
-		{
-            m_CanJump = true;
-            Debug.Log("guarda posicion" + previousPosition);
-            previousPosition = transform.position;
-        }
-
-        if (collision.gameObject.tag == "Object")
+        if (collision.gameObject.CompareTag("Terrain") || collision.gameObject.CompareTag("ProceduralTerrain"))
         {
-         
-            
-            transform.position = previousPosition;
-            Debug.Log("se movio hacia atras" + transform.position);
+            m_CanJump = true;
         }
     }
 }
+
